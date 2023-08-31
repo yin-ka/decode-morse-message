5593512e771a351f2a332ebcb7157a55d54e00ba
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-08-31 15:33:55 +0100

    Fix linters errors

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,37 +1,37 @@
 def decode_char(morse_code)
-    # Define a hash of Morse code characters and their corresponding uppercase letters
-    morse_dic = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-      '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-      '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-      '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-      '--..' => 'Z'
-    }
-    # Return the corresponding uppercase letter for the given Morse code character
-    morse_dic[morse_code]
-  end
+  # Define a hash of Morse code characters and their corresponding uppercase letters
+  morse_dic = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  # Return the corresponding uppercase letter for the given Morse code character
+  morse_dic[morse_code]
+end
 
-  def decode_word(morse_word)
-    str = ''
-    splited_arr = morse_word.split
-    splited_arr.each do |character|
-      str += decode_char(character)
-    end
-    str
+def decode_word(morse_word)
+  str = ''
+  splited_arr = morse_word.split
+  splited_arr.each do |character|
+    str += decode_char(character)
   end
-  
-  def decode_sentence(morse_sent)
-    str = ''
-    splited_arr = morse_sent.split('   ')
-    splited_arr.each do |sentence|
-      str += decode_word(sentence)
-      str += ' '
-    end
-    str.strip
-  end
+  str
+end
 
-  puts decode_char('.-')
-  puts decode_word('-- -.--')
-  puts decode_sentence('-- -.--   -. .- -- .')
-  puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+def decode_sentence(morse_sent)
+  str = ''
+  splited_arr = morse_sent.split('   ')
+  splited_arr.each do |sentence|
+    str += decode_word(sentence)
+    str += ' '
+  end
+  str.strip
+end
+
+puts decode_char('.-')
+puts decode_word('-- -.--')
+puts decode_sentence('-- -.--   -. .- -- .')
+puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
